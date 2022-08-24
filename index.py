282a234a02ba4aea3ce40ebd2b6cf8831d9e9472
Stefano Assenzo <stef58a@gmail.com>
2022-08-24 12:34:06 +0200

    YEA I FIX IT

--- index.py
+++ index.py
@@ -1,12 +1,11 @@
-#Every 10s check if there are new games
 import requests
-#Libraries for Windows notification
+
 from win10toast import ToastNotifier
 
 url = "https://api.plenusbot.xyz/epic_games?country=IT"
 response = requests.get(url).json()
 # Title of current games
-current_games = response['currentGames'][0]['title']
+current_games_title = response['currentGames'][0]['title']
 
-toaster = ToastNotifier()
-toaster.show_toast("New free game on the Epic Games Store:", current_games, icon_path="epicgames.png")
+toast = ToastNotifier()
+toast.show_toast(title="New games!", msg="There is a new game on Epic Games Store " + current_games_title , icon_path="")
