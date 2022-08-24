54567bb204f0a68e35f5966952017f9887eea243
Stefano Assenzo <stef58a@gmail.com>
2022-08-24 14:14:11 +0200

    Finaly work!

--- index.py
+++ index.py
@@ -2,10 +2,11 @@ import requests
 
 from win10toast import ToastNotifier
 
+filename = 'epic_games.ico'
 url = "https://api.plenusbot.xyz/epic_games?country=IT"
 response = requests.get(url).json()
 # Title of current games
 current_games_title = response['currentGames'][0]['title']
 
 toast = ToastNotifier()
-toast.show_toast(title="New games!", msg="There is a new game on Epic Games Store " + current_games_title , icon_path="")
+toast.show_toast(title="New games!", msg="There is a new game on Epic Games Store " + current_games_title , icon_path=filename,duration=10,threaded=False)
