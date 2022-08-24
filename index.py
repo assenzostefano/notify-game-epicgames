f2d83f1295989cb1e1ed583d5a2cb74e1719717d
Stefano Assenzo <stef58a@gmail.com>
2022-08-24 12:13:22 +0200

    The message appears on Windows but cannot find the image

--- index.py
+++ index.py
@@ -1,3 +1,12 @@
+#Every 10s check if there are new games
+import requests
+#Libraries for Windows notification
 from win10toast import ToastNotifier
+
+url = "https://api.plenusbot.xyz/epic_games?country=IT"
+response = requests.get(url).json()
+# Title of current games
+current_games = response['currentGames'][0]['title']
+
 toaster = ToastNotifier()
-toaster.show_toast("Sample Notification","Python is awesome!!!")
+toaster.show_toast("New free game on the Epic Games Store:", current_games, icon_path="epicgames.png")
