87145d1058891055836fed0b72680956c9ab93c5
Stefano Assenzo <stef58a@gmail.com>
2022-08-25 17:40:00 +0200

    Now check if you have already redeemed the game.

--- index.py
+++ index.py
@@ -3,33 +3,58 @@ import requests
 #Create Windows notification
 from win10toast import ToastNotifier
 
-#URL API
-url = "https://api.plenusbot.xyz/epic_games?country=IT"
-response = requests.get(url).json()
-# Title of current games
-current_games_title1 = response['currentGames'][0]['title']
-current_games_title2 = response['currentGames'][1]['title']
-fix_1 = "["+"'"+current_games_title1+"'""]"
-fix_2 = "["+"'"+current_games_title2+"'""]"
+import time
 
-#Generate Windows notification
-def notification():
+def recheck_game():
+    #In 10 seconds I will double check if you have already redeemed the game.
+    time.sleep(10)
+    check_game()
+
+def send_notification1():
+    print("Send first game notification")
     #Icon file
     filename = 'epic_games.ico'
     #Create notification on Windows
     toast = ToastNotifier()
     toast.show_toast(title="New games!", msg="The new game is: " + current_games_title1 , icon_path=filename,duration=10,threaded=False)
+    with open('game1.txt', 'w') as f:
+        f.write(current_games_title1)
 
-#Check if i already redeem the game
-with open('game1.txt') as f:
-    lines = f.readlines()
-    if fix_1 == lines:
-        notification()
+def send_notification2():
+    print("Send second game notification")
+    #Icon file
+    filename = 'epic_games.ico'
+    #Create notification on Windows
+    toast = ToastNotifier()
+    toast.show_toast(title="New games!", msg="The new game is: " + current_games_title2 , icon_path=filename,duration=10,threaded=False)
+    with open('game2.txt', 'w') as f:
+        f.write(current_games_title2)
+
+
+def check_game():
+    print("Connect to the api")
+    url = "https://api.plenusbot.xyz/epic_games?country=IT"
+    response = requests.get(url).json()
+    # Title of current games
+    global current_games_title1
+    current_games_title1 = response['currentGames'][0]['title']
+    global current_games_title2
+    current_games_title2 = response['currentGames'][1]['title']
+    #Check first game
+    print("Check if you have already redeemed the game - First game")
+    f1 = open("game1.txt", "r")
+    if f1.read() == current_games_title1:
+        recheck_game()
     else:
-        print("Nope")
-with open('game2.txt') as f:
-    lines = f.readlines()
-    if fix_2 == lines:
-        notification()
+        print("Send notification for first game")
+        send_notification1()
+    #Check second game
+    print("Check if you have already redeemed the game - Second game")
+    f2 = open("game2.txt", "r")
+    if f2.read() == current_games_title2:
+        recheck_game()
     else:
-        print("Nope")
+        print("Send notification for second game")
+        send_notification2()
+
+check_game()
